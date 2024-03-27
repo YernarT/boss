f9a723c26a05a428ec5c74b5fda35ba65813d0ec
YernarT <toktaryernar@gmail.com>
2024-03-27 18:19:06 +0500

    update: :hammer: add address box

--- src/components/layout/default/HeaderComp.vue
+++ src/components/layout/default/HeaderComp.vue
@@ -1,110 +1,126 @@
 <template>
-    <header class="header">
-        <div class="itisit-container inner">
-            <div class="logo">
-                <NuxtLink to="/">BOSS Сұхбат</NuxtLink>
-            </div>
+  <header class="header">
+    <div class="itisit-container inner">
+      <div class="logo">
+        <NuxtLink to="/">BOSS Сұхбат</NuxtLink>
+      </div>
 
-            <ul class="inner__items">
-                <NuxtLink v-for="item in headerItems" :key="item.key" class="item" :to="item.url">
-                    {{ item.label }}
-                </NuxtLink>
-            </ul>
+      <ClientOnly>
+        <Address></Address>
+      </ClientOnly>
 
-            <ClientOnly>
-                <div class="actions">
-                    <a-button>Жұмыс іздеймін</a-button>
-                    <a-button>Жұмысшы іздеймін</a-button>
-                </div>
-            </ClientOnly>
+      <ul class="inner__items">
+        <NuxtLink
+          v-for="item in headerItems"
+          :key="item.key"
+          class="item"
+          :to="item.url"
+        >
+          {{ item.label }}
+        </NuxtLink>
+      </ul>
+
+      <ClientOnly>
+        <div class="actions">
+          <a-button>Жұмыс іздеймін</a-button>
+          <a-button>Жұмысшы іздеймін</a-button>
         </div>
-    </header>
+      </ClientOnly>
+    </div>
+  </header>
 
-    <div class="header-helper"></div>
+  <div class="header-helper"></div>
 </template>
 
 <script setup lang="ts">
 // Vue
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+// Components
+import Address from "~/components/layout/default/Address.vue";
 // Constants
-import { headerItems } from '~/constants/layout/default-layout-header';
+import { headerItems } from "~/constants/layout/default-layout-header";
 
-defineComponent({ name: 'HeaderComp' });
+defineComponent({ name: "HeaderComp" });
 </script>
 
 <style scoped lang="scss">
-@import '~/assets/style/mixins.scss';
+@import "~/assets/style/mixins.scss";
 
 .header {
-    width: 100%;
-    background-color: #202329;
-    box-shadow: 0 1px 8px rgba(0, 0, 0, .06);
-    @include positioned($position: fixed, $top: 0, $left: 0);
+  width: 100%;
+  background-color: #202329;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.06);
+  @include positioned($position: fixed, $top: 0, $left: 0);
 
-    .inner {
-        height: 50px;
-        position: relative;
-        @include flex($alignItems: center);
+  .inner {
+    height: 50px;
+    position: relative;
+    @include flex($alignItems: center);
 
-        &::before {
-            content: "";
-            width: 100%;
-            height: 50px;
-            background: url('~/assets/image/layout/default-layout-header-bg.png') 0 0 / auto 52px no-repeat;
-            @include positioned($top: 0, $left: calc(4% + 24px), $zIndex: -1);
-        }
-
-        .logo {
-            a {
-                font-weight: 500;
-                color: var(--c-primary);
-                text-decoration: none;
-                transition: text-shadow var(--transition);
-
-                &:hover {
-                    text-shadow: 0 0 2px var(--c-primary);
-                }
-            }
-        }
-
-        &__items {
-            margin-left: 24px;
-            @include flex($alignItems: center);
-
-            .item {
-                margin: 0 4px;
-                padding: 0 6px;
-                font-size: 14px;
-                color: #fff;
-                text-decoration: none;
-                transition: color var(--transition);
-
-                &:hover {
-                    color: var(--c-primary);
-                }
-            }
-
-            .router-link-exact-active {
-                color: var(--c-primary);
-            }
-        }
-
-        .actions {
-            margin-left: auto;
-            @include flex($alignItems: center, $gap: 8px);
-
-            :deep(.ant-btn) {
-                @include flexCenter;
-
-                span {
-                    font-size: 14px;
-                }
-            }
-        }
+    &::before {
+      content: "";
+      width: 100%;
+      height: 50px;
+      background: url("~/assets/image/layout/default-layout-header-bg.png") 0 0 /
+        auto 52px no-repeat;
+      @include positioned($top: 0, $left: calc(4% + 24px), $zIndex: -1);
     }
+
+    :deep(.address) {
+      margin-left: 24px;
+      margin-right: 8px;
+    }
+
+    .logo {
+      a {
+        font-weight: 500;
+        color: var(--c-primary);
+        text-decoration: none;
+        transition: text-shadow var(--transition);
+
+        &:hover {
+          text-shadow: 0 0 2px var(--c-primary);
+        }
+      }
+    }
+
+    &__items {
+      @include flex($alignItems: center);
+
+      .item {
+        margin: 0 4px;
+        padding: 0 6px;
+        font-size: 14px;
+        color: #fff;
+        text-decoration: none;
+        transition: color var(--transition);
+
+        &:hover {
+          color: var(--c-primary);
+        }
+      }
+
+      .router-link-exact-active {
+        color: var(--c-primary);
+      }
+    }
+
+    .actions {
+      margin-left: auto;
+      @include flex($alignItems: center, $gap: 8px);
+
+      :deep(.ant-btn) {
+        @include flexCenter;
+
+        span {
+          font-size: 14px;
+        }
+      }
+    }
+  }
 }
 
 .header-helper {
-    height: 50px;
+  height: 50px;
 }
 </style>
