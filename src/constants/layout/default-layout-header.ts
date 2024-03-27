d75f640e92bfedcb73b387d1ed7d99317d572979
YernarT <toktaryernar@gmail.com>
2024-03-27 18:18:23 +0500

    init: :rainbow: header items

---
+++ src/constants/layout/default-layout-header.ts
@@ -0,0 +1,22 @@
+export const headerItems = [
+  {
+    key: "home",
+    label: "Бастапқы",
+    url: "/",
+  },
+  {
+    key: "search",
+    label: "Іздеу",
+    url: "/search",
+  },
+  {
+    key: "company",
+    label: "Компания",
+    url: "/company",
+  },
+  {
+    key: "discuss",
+    label: "Талқы",
+    url: "/discuss",
+  },
+];
